c466e907150275da936fa9e10d37d8fa0f4c1426
cclauss <cclauss@bluewin.ch>
2016-07-07 11:10:03 +0200

    Simplify with str

--- datatypes.py
+++ datatypes.py
@@ -1,11 +1,9 @@
-
 from collections import namedtuple
 
+Position = namedtuple("Position", "x y")
 
-Position = namedtuple("Position", ["x", "y"])
+Vector = namedtuple("Vector", "xdir ydir")
 
-Vector = namedtuple("Vector", ["xdir", "ydir"])
+Char = namedtuple("Char", "char color")
 
-Char = namedtuple("Char", ["char", "color"])
-
-Draw = namedtuple("Draw", ["x", "y", "char", "color"])
+Draw = namedtuple("Draw",  "x y char color"])
